5caf2040d4aab945319d9fe6944ca46a9b04332c
unknown <maartinmxxx@gmail.com>
2019-10-10 16:49:44 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const MyApp = () => (
   <div>
   <h1>Minimal React mrugalam</h1>
+  <p>Bundle size: 59 bytes, Load time of the bundle: 45 ms, Last commit SHA1: 3a13c4fae846b156ce9e5efac71c241b1eb5f84c</p>
+
   </div>
 )
 
