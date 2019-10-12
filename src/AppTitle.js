5c4101665998e2cbe1feb5d6e0505419cd41f815
unknown <maartinmxxx@gmail.com>
2019-10-12 11:11:45 +0200

    Lab1Ex2

--- src/AppTitle.js
+++ src/AppTitle.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const AppTitle = () => (
-  <div>
-  <h1>Minimal React mrugalam2</h1>
-  </div>
+    <div>
+    <h1>New component.<br/>This is the end of the website.</h1>
+    </div>
 )
 
 export default AppTitle
--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+const generateArray=(n)=>
+(
+Array.from({length: n}, (v, k) => k+1)
+)
+
 const MyApp = () => (
   <div>
   <h1>Minimal React mrugalam</h1>
   <p>Bundle size: 59 bytes, Load time of the bundle: 114 ms, Last commit SHA1: d422c3021f335fa2154cd4d798cdbb4aaea8ab88</p>
+  <ul>
+    <p>An array of integers from 1 to n=10</p>
+      {generateArray(10).map(element => <li>{element}</li>)}
+
+    </ul>
 
   </div>)
 
