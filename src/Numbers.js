9293dab22db93bcb627489b01fe2c77643307e12
unknown <maartinmxxx@gmail.com>
2019-10-17 22:02:08 +0200

    Lab2Ex2

--- src/Numbers.js
+++ src/Numbers.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-var handler=()=>{
-    console.log("This text comes from arrow function outside the class")
-}
 
 
 class Numbers extends React.Component {
@@ -22,13 +19,37 @@ this.bFieldValueChangeHandler=this.bFieldValueChangeHandler.bind(this)
 aFieldValueChangeHandler(ev)
 {
     console.log("Value changed a:"+ev.target.value)
-    this.setState({a:ev.target.value},(nextState)=>(console.log()));
+    this.generateArray(ev.target.value,this.state.b)
+    this.setState({a:ev.target.value});
 }
 bFieldValueChangeHandler(ev)
 {
     console.log("Value changed b:"+ev.target.value)
-    this.setState({b:ev.target.value},(nextState)=>(console.log()));
+    this.generateArray(this.state.a,ev.target.value)
+    this.setState({b:ev.target.value});
+    
 }
+
+generateArray(a,b)
+{
+    document.getElementById('listID').innerHTML = ""
+    a=parseInt(a)
+    b=parseInt(b)
+    if(a<b&&a>0)
+    {
+        var array=new Array()
+        var i=a+1
+        var j
+        for(j=0;j<b-a-1;i++,j++)
+        {
+            array[j]=i;
+        }
+
+        array.forEach(element=>document.getElementById('listID').innerHTML += "<li>"+element+"</li>")
+    }
+    
+}
+
   render() {
     return (
         <div>
@@ -38,6 +59,7 @@ bFieldValueChangeHandler(ev)
        <br/>
        <label>Number b: </label>
        <input type="number" onChange={this.bFieldValueChangeHandler} />
+       <ul id="listID"/>
         </div>
     );
   }
