60bc5ecf7f641292199a64b9865e96db111c2139
unknown <maartinmxxx@gmail.com>
2019-10-18 22:36:06 +0200

    Lab2Ex2

--- src/Numbers.js
+++ src/Numbers.js
@@ -10,6 +10,7 @@ super(props);
 this.state={
     a:0,
    b:0,
+   array:[],
 
 }
 
@@ -18,6 +19,7 @@ this.state={
 this.aFieldValueChangeHandler=this.aFieldValueChangeHandler.bind(this)
 this.bFieldValueChangeHandler=this.bFieldValueChangeHandler.bind(this)
 
+this.generateArray=this.generateArray.bind(this)
 
 }
 
@@ -28,7 +30,7 @@ aFieldValueChangeHandler(ev)
     console.log("Value changed a:"+ev.target.value)
     
     this.setState({a:ev.target.value});
-
+    this.generateArray(ev.target.value,this.state.b)
 }
 bFieldValueChangeHandler(ev)
 {
@@ -36,15 +38,34 @@ bFieldValueChangeHandler(ev)
     console.log("Value changed b:"+ev.target.value)
    
     this.setState({b:ev.target.value});
-
+    this.generateArray(this.state.a,ev.target.value)
     
+}
+
+generateArray(a,b)
+{
+    a=parseInt(a)
+    b=parseInt(b)
+    if(a<b&&a>0)
+    {
+        this.state.array=[]
+        var i=a+1
+        var j
+        for(j=0;j<b-a-1;i++,j++)
+        {
+            this.state.array[j]=i;
+        }
+    }
+    else
+    {
+        this.state.array=[]
+    }
+
 }
 
 
 
-
   render() {
-
     const content=(
         <div>
         <label>Number a: </label>
@@ -53,7 +74,10 @@ bFieldValueChangeHandler(ev)
        <br/>
        <label>Number b: </label>
        <input type="number" onChange={this.bFieldValueChangeHandler} />
-
+       <ul>
+          {this.state.array.map(item => (
+            <li key={item}>{item}</li>))}
+        </ul>
 
         </div>
     )
